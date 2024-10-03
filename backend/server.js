5ace75305aecd182000bc09cad18b6335469d310
AnwarDev2 <Anwarabdi2090@gmail.com>
2024-10-03 21:54:27 +0300

    changes on server

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,14 @@ dotenv.config();
 
 const app = express();
 
+const PORT = process.env.PORT || 5000
+
 app.use(express.json());
 
 app.use("/api/products",productRoutes);
 
 // Start the server and connect to the database
-app.listen(5000, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log('Server started at http://localhost:5000');
+  console.log('Server started at http://localhost:' + PORT);
 });
