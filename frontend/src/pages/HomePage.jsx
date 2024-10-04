767fab73452fc26d2968e038769c6d8d4e4c37b9
AnwarDev2 <Anwarabdi2090@gmail.com>
2024-10-04 15:08:15 +0300

    updated home pages

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,31 @@
+import {Container,VStack,Text,} from '@chakra-ui/react';
+import {Link} from 'react-router-dom';
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <Container maxW="container.xl" py={12}>
+      <VStack spacing={8}>
+        <Text
+          fontSize={{ base: '22', sm: '28' }}
+          fontWeight={'bold'}
+          textTransform={'uppercase'}
+          textAlign={'center'}
+          bgGradient={'linear(to-r,cyan.400,blue.500)'}
+        >
+          Current Products
+        </Text>
+
+        <Text fontSize={'xl'} textAlign={"center"} fontWeight={'bold'} color='gray.500'>
+          No products found😓{""}
+          <Link to ={"/create"}>
+          <Text as='span' color='blue.500' _hover={{textDecoration:"underline"}}>
+             Create a Product
+          </Text> 
+          </Link>
+        </Text>
+      </VStack>
+    </Container>
+  );
+};
 
 export default HomePage;
